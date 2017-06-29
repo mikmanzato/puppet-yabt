391892bce264fd7669aa335b8afc495fd88c9603
Michele Manzato <michele.manzato@newtargetweb.it>
2017-06-29 23:48:46 +0200

    - yabt version 1.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
-## 2017-06-16 Michele Manzato <michele.manzato@gmail.com> - 0.1.0
+
+## 2016-06-16 Michele Manzato <michele.manzato@gmail.com> - 0.1.0
 * Initial release under puppetlabs
--- manifests/config.pp
+++ manifests/config.pp
@@ -3,6 +3,6 @@
 class yabt::config {
     $etc_dir = '/usr/local/etc/yabt'
     $jobs_dir = "${etc_dir}/jobs.d"
-    $latest_version = '1.1.2'
+    $latest_version = '1.1.3'
     $releases_url = 'https://github.com/mikmanzato/yabt/releases/download/'
 }
